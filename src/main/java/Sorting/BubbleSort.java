870cb4a29d7360b1b4f7a37c9812bf9fe9da6cb0
padminimandal-babylon <padminiannigeri@icloud.com>
2021-01-11 21:32:58 -0600

    Added Comments to BuubleSort

--- src/main/java/Sorting/BubbleSort.java
+++ src/main/java/Sorting/BubbleSort.java
@@ -2,6 +2,20 @@ package Sorting;
 
 import util.Print;
 
+/**
+ * Algorithm - All about getting the bigger value bubble up to the right side of the array.
+ * 1. Iterate over the array 0 to n. Consider 6 values, integers in this case.
+ *    Ex: 0, n-5, n-4, n-3, n-2, n-1, n
+ * 2. Compare 2 elements adjacent each time and swap the bigger element to the right.
+ *     (0, n-5), (n-5, n-4), (n-4, n-3), (n-3, n-2), (n-2, n-1), (n-1, n)
+ *     At this point nth element should be the largest.
+ * 3. After 1 iteration nth element will be the biggest element in the array.
+ *    Hence in next iteration ignore nth element in 2nd ignore n-1.
+ * 4. Add a boolean to check if there was swap in inner loop iteration. If no then the array is sorted.
+ *
+ * Outer for loop starts at the n length and decrements to 0
+ * Inner loop starts at 0 and increments till outer loop value.
+ */
 public class BubbleSort {
     public static void main(String[] args) {
         int[] data = new int[]{1,4,7,3,9};
@@ -11,6 +25,12 @@ public class BubbleSort {
         Print.printArray(data, 0);
         Print.printArray(bubbleSort(data), 1);
     }
+
+    /**
+     * This method implement the algorithm to sort an Array using Buuble Sort algorithm.
+     * @param data - Unsorted array of elements
+     * @return Sorted array in descending order.
+     */
     public static int[] bubbleSort(int[] data) {
 
         for (int i = data.length - 1; i >= 0; i--) {
@@ -23,6 +43,12 @@ public class BubbleSort {
         return data;
     }
 
+    /**
+     * This method swaps the element on the array at the given index.
+     * @param data - Array of Integers
+     * @param left - Left index
+     * @param right - Right index
+     */
     private static void swap(int[] data, int left, int right) {
         int temp = data[left];
         data[left] = data[right];
--- src/main/java/util/Print.java
+++ src/main/java/util/Print.java
@@ -3,11 +3,11 @@ package util;
 public class Print {
     public static void printArray(int[] data, int type) {
         if(type == 0) {
-            System.out.println("Input array: ");
+            System.out.print("Input array : ");
         } else if(type == 1){
-            System.out.println("Output array: ");
+            System.out.print("Output array: ");
         }else if(type == 2){
-            System.out.println("Mid array: ");
+            System.out.print("Mid array   : ");
         }
         System.out.print("[");
         for (int i = 0; i < data.length; i++) {
